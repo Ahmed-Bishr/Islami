c16f53d256bea7af70693e5812177bb54abdd629
Ahmed746996 <70495039+Ahmed746996@users.noreply.github.com>
2024-01-21 21:02:16 +0200

    initial code for islami project

---
+++ app/src/main/java/com/example/islami/splashscreen.kt
@@ -0,0 +1,25 @@
+package com.example.islami
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splashscreen : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
