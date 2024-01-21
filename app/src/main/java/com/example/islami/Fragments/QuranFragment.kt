9ddf7990487e3e00750ede4820eaa5ce4564a40c
Ahmed746996 <70495039+Ahmed746996@users.noreply.github.com>
2024-01-22 01:21:22 +0200

    finishing yhe next part of the navigation bar
    few Updates

--- app/src/main/java/com/example/islami/Fragments/QuranFragment.kt
+++ app/src/main/java/com/example/islami/Fragments/QuranFragment.kt
@@ -6,13 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.islami.Adaptor
+import com.example.islami.data
 
 import com.example.islami.databinding.ActivityQuranFragmentBinding
+import com.example.islami.suraNamesList
 
-class QuranFragment : Fragment(){
+class QuranFragment : Fragment() {
 
     lateinit var binding: ActivityQuranFragmentBinding
-
+    lateinit var adaptor: Adaptor
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,8 +25,14 @@ class QuranFragment : Fragment(){
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val suraList = suraNamesList.mapIndexed { position, item ->
+            data(item, position + 1)
+        }
+
+        adaptor = Adaptor(suraList)
+        binding.suraView.adapter = adaptor
     }
 
 
